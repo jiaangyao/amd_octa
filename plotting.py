bb3c5cb699d00d8ae90a75b2187f2ff1ab564fb2
jyao <jy2951@columbia.edu>
2020-10-25 17:52:15 -0400

    fixed plotting for raw confusion matrix

--- plotting.py
+++ plotting.py
@@ -17,9 +17,10 @@ def plot_training_loss(h, cfg, save=True):
     if save:
         f_figure = 'training_loss.eps'
         fig.savefig(str(cfg.p_figure / f_figure), format='eps')
+
+        plt.close(fig)
     else:
         plt.show()
-    plt.close(fig)
 
 
 def plot_training_acc(h, cfg, save=True):
@@ -34,12 +35,13 @@ def plot_training_acc(h, cfg, save=True):
     if save:
         f_figure = 'training_acc.eps'
         fig.savefig(str(cfg.p_figure / f_figure), format='eps')
+
+        plt.close(fig)
     else:
         plt.show()
-    plt.close(fig)
 
 
-def plot_norm_conf_matrix(y_true, y_pred, cfg, save=True):
+def plot_norm_conf_matrix(y_true, y_pred, cfg, save=True, f_figure=None):
     conf_matrix = confusion_matrix(y_true, y_pred)
 
     # plot the confusion matrix
@@ -54,20 +56,30 @@ def plot_norm_conf_matrix(y_true, y_pred, cfg, save=True):
         'ytick.labelsize': 13,
         'text.usetex': False,
     }
+
+    # params = {
+    #     'font.size': 16,
+    #     'axes.titlesize': 16,
+    #     'axes.labelsize': 16,
+    #     'xtick.labelsize': 16,
+    #     'ytick.labelsize': 16,
+    #     'text.usetex': False,
+    # }
     rcParams.update(params)
 
     cmap = plt.get_cmap('Blues')
     plt.figure(figsize=(8, 6))
     plt.imshow(conf_matrix_norm, interpolation='nearest', cmap=cmap)
+    plt.colorbar()
 
     if cfg.str_model == 'arch_009':
-        str_title = 'Normalized Confusion Matrix: {}'.format('Angiography')
-    elif cfg.str_model == 'arch_010':
         str_title = 'Normalized Confusion Matrix: {}'.format('OCTA')
-    elif cfg.str_model == 'arch_012' or cfg.str_model == 'arch_013':
+    elif cfg.str_model == 'arch_010':
+        str_title = 'Normalized Confusion Matrix: {}'.format('OCTA+OCT')
+    elif cfg.str_model == 'arch_012' or cfg.str_model == 'arch_013' or cfg.str_model == 'arch_023':
         str_title = 'Normalized Confusion Matrix: {}'.format('B-scan')
     elif cfg.str_model == 'arch_022':
-        str_title = 'Normalized Confusion Matrix: {}'.format('OCTA+B-scan')
+        str_title = 'Normalized Confusion Matrix: {}'.format('OCTA+OCT+B-scan')
     else:
         str_title = 'Normalized Confusion Matrix'
 
@@ -92,15 +104,23 @@ def plot_norm_conf_matrix(y_true, y_pred, cfg, save=True):
 
     fig = plt.gcf()
     if save:
-        f_figure = 'norm_conf_matrix.eps'
+        if f_figure is not None:
+            f_figure = 'norm_conf_matrix_{}.eps'.format(f_figure)
+        else:
+            f_figure = 'norm_conf_matrix.eps'
         fig.savefig(str(cfg.p_figure / f_figure), format='eps')
+        plt.close(fig)
     else:
         plt.show()
-    plt.close(fig)
 
 
-def plot_raw_conf_matrix(y_true, y_pred, cfg, save=True):
+def plot_raw_conf_matrix(y_true, y_pred, cfg, save=True, f_figure=None):
     conf_matrix = confusion_matrix(y_true, y_pred)
+    conf_matrix_copy = np.array(conf_matrix, copy=True)
+
+    # plot the confusion matrix
+    # normalize the matrix first
+    conf_matrix_norm = conf_matrix_copy.astype('float') / conf_matrix_copy.sum(axis=1)[:, np.newaxis]
 
     params = {
         'font.size': 15,
@@ -110,31 +130,41 @@ def plot_raw_conf_matrix(y_true, y_pred, cfg, save=True):
         'ytick.labelsize': 13,
         'text.usetex': False,
     }
+
+    # params = {
+    #     'font.size': 16,
+    #     'axes.titlesize': 16,
+    #     'axes.labelsize': 16,
+    #     'xtick.labelsize': 16,
+    #     'ytick.labelsize': 16,
+    #     'text.usetex': False,
+    # }
     rcParams.update(params)
 
     cmap = plt.get_cmap('Blues')
     plt.figure(figsize=(8, 6))
-    plt.imshow(conf_matrix, interpolation='nearest', cmap=cmap)
+    plt.imshow(conf_matrix_norm, interpolation='nearest', cmap=cmap)
+    plt.colorbar()
 
     if cfg.str_model == 'arch_009':
-        str_title = 'Raw Confusion Matrix: {}'.format('Angiography')
-    elif cfg.str_model == 'arch_010':
         str_title = 'Raw Confusion Matrix: {}'.format('OCTA')
-    elif cfg.str_model == 'arch_012' or cfg.str_model == 'arch_013':
+    elif cfg.str_model == 'arch_010':
+        str_title = 'Raw Confusion Matrix: {}'.format('OCTA+OCT')
+    elif cfg.str_model == 'arch_012' or cfg.str_model == 'arch_013' or cfg.str_model == 'arch_023':
         str_title = 'Raw Confusion Matrix: {}'.format('B-scan')
     elif cfg.str_model == 'arch_022':
-        str_title = 'Raw Confusion Matrix: {}'.format('OCTA+B-scan')
+        str_title = 'Raw Confusion Matrix: {}'.format('OCTA+OCT+B-scan')
     else:
         str_title = 'Raw Confusion Matrix'
 
     plt.title(str_title)
 
-    thresh = np.max(conf_matrix) / 1.5
+    thresh = np.max(conf_matrix_norm) / 1.5
     for i, j in itertools.product(range(conf_matrix.shape[0]),
                                   range(conf_matrix.shape[1])):
         plt.text(j, i, "{}".format(conf_matrix[i, j]),
                  horizontalalignment="center",
-                 color="white" if conf_matrix[i, j] > thresh else "black")
+                 color="white" if conf_matrix_norm[i, j] > thresh else "black")
 
     plt.tight_layout()
 
@@ -148,8 +178,13 @@ def plot_raw_conf_matrix(y_true, y_pred, cfg, save=True):
 
     fig = plt.gcf()
     if save:
-        f_figure = 'raw_conf_matrix.eps'
+        if f_figure is not None:
+            f_figure = 'raw_conf_matrix_{}.eps'.format(f_figure)
+        else:
+            f_figure = 'raw_conf_matrix.eps'
         fig.savefig(str(cfg.p_figure / f_figure), format='eps')
+        plt.close(fig)
+
     else:
         plt.show()
-    plt.close(fig)
+
